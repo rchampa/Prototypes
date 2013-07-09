26935f6a927e4ad142be9407238c465f0e0afb64
rchampa <ricardocb48@gmail.com>
2013-07-09 04:49:11 +0200

    Graphhopper added.

---
+++ Graphhoper/graphhopper-android-0.1/src/com/graphhopper/android/MyAsyncTask.java
@@ -0,0 +1,28 @@
+package com.graphhopper.android;
+
+import android.os.AsyncTask;
+
+public abstract class MyAsyncTask<A, B, C> extends AsyncTask<A, B, C> {
+	private Throwable error;
+
+	protected abstract C saveDoInBackground(A... params) throws Exception;
+		
+	protected C doInBackground(A... params) {
+		try {
+			return saveDoInBackground(params);
+		} catch (Throwable t) {
+			error = t;
+			return null;
+		}
+	};
+
+	public boolean hasError() {
+		return error != null;
+	}
+	
+	public String getErrorMessage() {
+		if(hasError()) 
+			return error.getMessage();
+		return "No Error";			
+	}
+}
