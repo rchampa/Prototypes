8fd8dc837b630c3d2c210af689f615a7c56aa137
rchampa <ricardocb48@gmail.com>
2013-12-01 17:21:10 +0100

    Version 1.0 released.

---
+++ Android/CicloVida/src/com/example/ciclovida/AnotherActivity.java
@@ -0,0 +1,19 @@
+package com.example.ciclovida;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherActivity extends Activity {
+	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_other);
+
+	}
+	
+	
+
+}
--- Android/CicloVida/src/com/example/ciclovida/MainActivity.java
+++ Android/CicloVida/src/com/example/ciclovida/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.ciclovida;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
 
-	StringBuilder builder = new StringBuilder();
-	TextView textView;
+	private StringBuilder builder = new StringBuilder();
+	private TextView textView;
+	private long timer;
+	private final long control = 1500;
+	
+	public void onClick(View v){
+		
+		if(v.getId()==R.id.button1){
+			Intent intent = new Intent(this, AnotherActivity.class);
+        	startActivity(intent);
+			
+			
+		}
+	}
 
-	private void log(String text) {
-		Log.d("Ciclo de vida de una activity", text);
+	protected void log(String text) {
+		
+		if(System.currentTimeMillis()-timer>control){
+			builder.append("----------------------------------");
+			builder.append('\n');
+		}
+		
+		Log.d("Activity lifecycle", text);
 		builder.append(text);
 		builder.append('\n');
 		textView.setText(builder.toString());		
 		
+		timer = System.currentTimeMillis();
 	}
 	
 	@Override
@@ -26,25 +48,46 @@ public class MainActivity extends Activity {
 		
 		textView = (TextView)findViewById(R.id.logTV);
 		
-		//textView = new TextView(this);
-		//textView.setText(builder.toString());
-		//setContentView(textView);
-		log("Estado: created");
+		log("State: created");
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		log("State: started");
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		log("Estado: resumed");
+		log("State: resumed");
 	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
-		log("Estado: paused");
+		log("State: paused");
 		if (isFinishing()) {
 			log("finishing");
 		}
 	}	
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		log("State: stopped");
+	}
+	
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		log("State: restarted");
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		log("State: destroyed");
+	}
 
 }
